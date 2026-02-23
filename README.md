b0363254cac08e3d3fe39f85f45470f2a32c5773
sheraz-khan-wazir <sherazwzr786@gmail.com>
2026-02-24 00:53:14 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Todo app, where you can create account and login and add task etc 
---
+++ js/Login.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login-form");
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+
+    //  validate credentials
+ let users = JSON.parse(localStorage.getItem("users")) || [];
+
+const user = users.find(user => user.email === email && user.password === password);
+
+if(user) {
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    alert("Login successful! Redirecting to dashboard.");
+    window.location.href = "dashboard.html";
+} else {
+    alert("Invalid email or password!");
+}
+
+
+});
